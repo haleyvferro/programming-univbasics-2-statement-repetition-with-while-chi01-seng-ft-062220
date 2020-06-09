c67bff3e423afe32da57bd0ff74b21e48b901098
Haley Ferro <haleyvferro@gmail.com>
2020-06-09 04:06:54 +0000

    Done.

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,1 +1,14 @@
-# Write your code here
+#magic_exit_number = 7
+#count = 0
+#while count < 10 && count != magic_exit_number do
+#  puts "I am the #{count}, I love to count!" # Work
+#  count = count + 1
+#end
+
+magic_exit_number = 0
+count = 10
+while count < 11 && count !=magic_exit_number do
+  puts "#{count}"
+  count = count - 1
+end
+puts "Happy New Year!"

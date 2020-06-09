78b020b95b92a3007a4d01e00c3c41379a190a1d
Haley Ferro <haleyvferro@gmail.com>
2020-06-09 03:47:53 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,6 +1,10 @@
-magic_exit_number = 7
-count = 0
-while count < 10 && count != magic_exit_number do
-  puts "I am the #{count}, I love to count!" # Work
-  count = count + 1
-end
+#magic_exit_number = 7
+#count = 0
+#while count < 10 && count != magic_exit_number do
+#  puts "I am the #{count}, I love to count!" # Work
+#  count = count + 1
+#end
+
+magic_exit_number = 0
+count = 10
+while count <

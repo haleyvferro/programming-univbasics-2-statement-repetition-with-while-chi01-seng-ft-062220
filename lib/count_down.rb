1a574742894c7d10322f87f30da231cecc16a4f6
Haley Ferro <haleyvferro@gmail.com>
2020-06-09 03:56:07 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -9,5 +9,7 @@ magic_exit_number = 0
 count = 10
 while count >0 && count != magic_exit_number do 
   puts {count}
+  break if count == magic_exit_number
+  puts "Happy New Year!"
   count = count - 1
 end
